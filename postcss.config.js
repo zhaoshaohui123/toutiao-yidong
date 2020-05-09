9d2448a74100cc5557647824054328df96580d4e
zhaoshaoh123 <1270694855@qq.com>
2020-05-09 11:49:17 +0800

    修改browserslistrc配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
-// postcss的配置文件
+// postcss的配置文件 基于nodejs 运行的一个处理css的工具
+// 所以他的配置文件也是运行在node.js中
 module.exports = {
+  // 配置要使用的相关插件
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // 自动添加浏览器厂商声明前缀,兼容不同的浏览器
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
+    // 把px转为rem
     'postcss-pxtorem': {
       rootValue: 37.5,
       propList: ['*']
