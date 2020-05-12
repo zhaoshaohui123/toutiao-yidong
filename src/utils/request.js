e8c686fa4ce5d37a63772c70e77efb7300d64d2c
zhaoshaoh123 <1270694855@qq.com>
2020-05-12 12:34:44 +0800

    我的-使用请求拦截器统一设置token

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,12 +2,24 @@
  * 请求模块
  */
 import axios from 'axios'
+// 在非组件模块中获取 store 必须通过这种方式
+// 这里单独加载 store，和在组件中 this.$store 一个东西
+import store from '@/store/'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 
