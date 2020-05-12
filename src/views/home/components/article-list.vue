8441f8bfb67f8ba1715c3c753e86cfbb52b4deb6
zhaoshaoh123 <1270694855@qq.com>
2020-05-12 19:02:29 +0800

    文章列表-初步完成

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="article-list">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleList',
+  components: {},
+  props: {
+    channel: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    onLoad () {
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        this.loading = false
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less"></style>
