1bda544f234c2b1fa6ef8f7ad6b43ef71ab02c46
zhaoshaoh123 <1270694855@qq.com>
2020-05-13 12:10:26 +0800

    首页文章列表显示出来初步完成

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="article-list">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isRefreshLoading"
+      :success-text="refreshSuccessText"
+      :success-duration="1500"
+      @refresh="onRefresh"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in articles"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
+import { getArticles } from '@/api/article'
 export default {
   name: 'ArticleList',
   components: {},
@@ -23,9 +35,12 @@ export default {
   },
   data () {
     return {
-      list: [],
-      loading: false,
-      finished: false
+      articles: [], // 数据列表
+      loading: false, // 控制加载中的 loading 状态
+      finished: false, // 控制加载结束的状态，当加载结束，不再触发加载更多
+      timestamp: null, // 获取下一页数据的时间戳
+      isRefreshLoading: false, // 下拉刷新的 loading 状态
+      refreshSuccessText: '' // 下拉刷新成功的提示文本
     }
   },
   computed: {},
@@ -33,19 +48,47 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onLoad () {
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        this.loading = false
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
+    async onLoad () {
+      const { data } = await getArticles({
+        channel_id: this.channel.id,
+        timestamp: this.timestamp || Date.now(),
+        with_top: 1
+      })
+      const { results } = data.data
+      this.articles.push(...results)
+      this.loading = false
+      if (results.length) {
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        this.finished = true
+      }
+    },
+    async onRefresh () {
+      // 下拉刷新，组件自己就会展示 loading 状态
+      // 1. 请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道 ID
+        timestamp: Date.now(), // 为了大家方便学习，只要你传递不同的时间戳就一定给你返回不一样的数据，而且数据不为空
+        with_top: 1
+      })
+      // 2. 把数据放到数据列表中（往顶部追加）
+      const { results } = data.data
+      this.articles.unshift(...results)
+      // 3. 关闭刷新的状态 loading
+      this.isRefreshLoading = false
+      this.refreshSuccessText = `更新了${results.length}条数据`
     }
   }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.article-list {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 50px;
+  top: 90px;
+  overflow-y: auto;
+}
+</style>
