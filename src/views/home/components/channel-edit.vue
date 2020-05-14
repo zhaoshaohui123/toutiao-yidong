6567c8f480417af6ce4801100f098eaed558763d
zhaoshaoh123 <1270694855@qq.com>
2020-05-14 12:53:42 +0800

    频道编辑进行中。。。

---
+++ src/views/home/components/channel-edit.vue
@@ -0,0 +1,158 @@
+<template>
+   <div class="channel-edit">
+    <van-cell center :border="false">
+      <div
+        slot="title"
+        class="channel-title"
+      >我的频道</div>
+      <van-button
+        type="danger"
+        plain
+        round
+        size="mini"
+       @click="isEdit = !isEdit"
+ >{{ isEdit ? '完成' : '编辑' }}</van-button>
+    </van-cell>
+
+    <van-grid :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        :icon="(isEdit && index !== 0) ? 'clear' : ''"
+        v-for="(channel, index) in userChannels"
+        :key="index"
+        :text="channel.name"
+        @click="onUserChannelClick(index)"
+      />
+    </van-grid>
+
+    <van-cell center :border="false">
+      <div
+        slot="title"
+        class="channel-title"
+      >频道推荐</div>
+    </van-cell>
+
+    <van-grid :gutter="10">
+      <van-grid-item
+        class="grid-item"
+        v-for="(channel, index)  in recommendChannels"
+        :key="index"
+        :text="channel.name"
+        @click="onAdd(channel)"
+      />
+    </van-grid>
+  </div>
+</template>
+
+<script>
+import { getAllChannels } from '@/api/channel'
+export default {
+  name: 'ChannelEdit',
+  components: {},
+  props: {
+    userChannels: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      allChannels: [], // 所有频道数据列表
+      isEdit: false // 控制编辑的显示状态
+    }
+  },
+  computed: {
+    // 推荐的频道列表
+    recommendChannels () {
+      // 思路：所有频道 - 我的频道 = 剩下的推荐频道
+      // filter 方法：过滤数据，根据方法返回的布尔值 true 来收集数据
+      // filter 方法查找满足条件的所有元素
+      return this.allChannels.filter(channel => {
+        // 判断 channel 是否属于用户频道
+        // find 方法查找满足条件的单个元素
+        return !this.userChannels.find(userChannel => {
+          // 找到满足该条件的元素
+          return userChannel.id === channel.id
+        })
+      })
+      // const arr = []
+      // // 遍历所有频道
+      // this.allChannels.forEach(channel => {
+      // let flag = false
+      // for (let i = 0; i < this.userChannels.length; i++) {
+      // if (this.userChannels[i].id === channel.id) {
+      // // 所有频道中的频道项属于用户频道
+      // flag = true
+      // break
+      //     }
+      //   }
+      // if (!flag) {
+      // arr.push(channel)
+      //   }
+      // })
+      // return arr
+    }
+  },
+  watch: {},
+  created () {
+    this.loadAllChannels()
+  },
+  mounted () {},
+  methods: {
+    async loadAllChannels () {
+      const { data } = await getAllChannels()
+      this.allChannels = data.data.channels
+    },
+    onAdd (channel) {
+      this.userChannels.push(channel)
+
+      // TODO: 数据持久化
+    },
+    onUserChannelClick (index) {
+      if (this.isEdit && index !== 0) {
+        // 编辑状态，删除频道
+        this.deleteChannel(index)
+      } else {
+        // 非编辑状态，切换频道
+        this.switchChannel(index)
+      }
+    },
+    deleteChannel (index) {
+      this.userChannels.splice(index, 1)
+      // 数据持久化
+    },
+    switchChannel (index) {
+      console.log('切换频道')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.channel-edit {
+  padding-top: 54px;
+    .channel-title {
+    font-size: 16px;
+    color: #333333;
+  }
+  .grid-item {
+    width: 80px;
+    height: 43px;
+    /deep/ .van-grid-item__content {
+      background-color: #f4f5f6;
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #222;
+        margin-top: 0;
+      }
+    }
+     /deep/ .van-grid-item__icon {
+      position: absolute;
+      right: -5px;
+      top: -5px;
+      font-size: 20px;
+      color: #ccc;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -16,7 +16,7 @@
      <!--
       标签页组件有一个功能，只有你第1次查看标签页的时候才会渲染里面的内容
      -->
-    <van-tabs v-model="active">
+    <van-tabs class="channel-tabs" v-model="active">
       <van-tab
         :title="channel.name"
         v-for="channel in channels"
@@ -26,24 +26,51 @@
         <article-list :channel="channel" />
         <!-- /文章列表 -->
       </van-tab>
+      <div
+        slot="nav-right"
+        class="wap-nav-placeholder"
+      ></div>
+      <div
+        slot="nav-right"
+        @click="isChannelEditShow = true"
+        class="wap-nav-wrap"
+      >
+        <van-icon name="wap-nav" />
+      </div>
     </van-tabs>
     <!-- /文章频道列表 -->
+    <van-popup
+      v-model="isChannelEditShow"
+      position="bottom"
+      class="channel-edit-popup"
+      closeable
+      close-icon-position="top-left"
+      get-container="body"
+      style="height: 100%"
+    >
+      <channel-edit
+        :user-channels="channels"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
+import ChannelEdit from './components/channel-edit'
 export default {
   name: 'HomeIndex',
   components: {
-    ArticleList
+    ArticleList,
+    ChannelEdit
   },
   props: {},
   data () {
     return {
       active: 0, // 控制被激活的标签
-      channels: [] // 频道列表
+      channels: [], // 频道列表
+      isChannelEditShow: true // 控制编辑频道的显示状态
     }
   },
   computed: {},
@@ -78,5 +105,45 @@ export default {
       font-size: 14px;
     }
   }
+    .channel-tabs {
+    /deep/ .van-tab {
+      border-right: 1px solid #edeff3;
+      border-bottom: 1px solid #edeff3;
+    }
+    /deep/ .van-tabs__line {
+      bottom: 20px;
+      width: 15px !important;
+      height: 3px;
+      background: #3296fa;
+    }
+  }
 }
+.wap-nav-placeholder {
+    width: 33px;
+    flex-shrink: 0;
+  }
+  .wap-nav-wrap {
+    position: fixed;
+    right: 0;
+    width: 33px;
+    height: 43px;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .9;
+    .van-icon {
+      font-size: 24px;
+    }
+    &:before {
+      content: '';
+      width: 1px;
+      background: url("") no-repeat;
+      background-size: contain;
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+    }
+  }
 </style>
