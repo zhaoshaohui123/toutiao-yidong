0806902d71d7c39428e769ddc0e67a07c9af33e5
zhaoshaoh123 <1270694855@qq.com>
2020-05-22 17:42:34 +0800

    编辑资料完成

--- src/router/index.js
+++ src/router/index.js
@@ -47,6 +47,11 @@ const routes = [
     component: () => import('@/views/article/'),
     // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
     props: true
+  },
+  {
+    path: '/user/profile',
+    name: 'user-profile',
+    component: () => import('@/views/user-profile')
   }
 ]
 
---
+++ src/views/user-profile/index.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <input type="file" accept="image/*" hidden ref="file" @change="onFileChange">
+    <van-cell title="头像" is-link center @click="$refs.file.click()">
+      <van-image
+        width="30"
+        height="30"
+        round
+        fit="cover"
+        :src="user.photo"
+      />
+    </van-cell>
+     <van-cell
+      title="昵称"
+      is-link
+      :value="user.name"
+      @click="isEditNameShow = true"
+    />
+    <van-cell
+      title="性别"
+      is-link
+      :value="user.gender === 0 ? '男' : '女'"
+      @click="isEditGenderShow  = true"
+    />
+    <van-cell
+      title="生日"
+      is-link
+      :value="user.birthday"
+      @click="isEditBirthdayShow  = true"
+    />
+    <van-popup
+      v-model="isEditNameShow"
+      position="bottom"
+      :style="{ height: '100%' }"
+    >
+      <update-name
+        v-model="user.name"
+        @close="isEditNameShow = false"
+      />
+    </van-popup>
+    <!-- 修改性别 -->
+    <van-popup
+      v-model="isEditGenderShow"
+      position="bottom"
+    >
+      <update-gender
+        v-model="user.gender"
+        @close="isEditGenderShow = false"
+      />
+    </van-popup>
+    <!-- /修改性别 -->
+    <!-- 修改生日 -->
+    <van-popup
+      v-model="isEditBirthdayShow"
+      position="bottom"
+    >
+      <update-birthday
+        v-if="isEditBirthdayShow"
+        v-model="user.birthday"
+        @close="isEditBirthdayShow = false"
+      />
+    </van-popup>
+    <!-- /修改生日 -->
+    <!-- 修改头像 -->
+    <van-popup
+      class="update-photo-popup"
+      v-model="isEditPhotoShow"
+      position="bottom"
+      style="height: 100%"
+    >
+      <update-photo
+        v-if="isEditPhotoShow"
+        :file="previewImage"
+        @close="isEditPhotoShow = false"
+        @update-photo="user.photo = $event"
+      />
+    </van-popup>
+    <!-- /修改头像 -->
+  </div>
+</template>
+
+<script>
+import { getUserProfile } from '@/api/user'
+import UpdateName from './components/update-name'
+import UpdateGender from './components/update-gender'
+import UpdateBirthday from './components/update-birthday'
+import UpdatePhoto from './components/update-photo'
+
+export default {
+  name: 'UserProfile',
+  components: {
+    UpdateName,
+    UpdateGender,
+    UpdateBirthday,
+    UpdatePhoto
+  },
+  props: {},
+  data () {
+    return {
+      user: {}, // 用户数据
+      isEditNameShow: false, // 编辑昵称的显示状态
+      isEditGenderShow: false, // 编辑性别的显示状态
+      isEditBirthdayShow: false, // 编辑生日的显示状态
+      isEditPhotoShow: false, // 编辑头像的显示状态
+      previewImage: null // 上传预览图片
+
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    this.loadUserProfile()
+  },
+  mounted () {},
+  methods: {
+    async loadUserProfile () {
+      const { data } = await getUserProfile()
+      this.user = data.data
+    },
+    onFileChange () {
+      // 展示弹出层
+      // 在弹出层里面预览图片
+      //  const blob = window.URL.createObjectURL(this.$refs.file.files[0])
+      //  this.previewImage = blob
+      const file = this.$refs.file.files[0]
+      this.previewImage = file
+      // 展示弹出层
+      this.isEditPhotoShow = true
+      // 为了解决相同文件不触发 change 事件，所以在这里手动的清空 file 的 value
+      this.$refs.file.value = ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.van-popup {
+  background-color: #f5f7f9;
+}
+.update-photo-popup {
+  background-color: #000;
+}
+</style>
